14117564131d2d8dc5f143bcad9c5be101c2549b
Jacob Brockwell <brockwell2001@gmail.com>
2023-10-05 11:38:34 -0500

    removed target blank to prevent webpage opening up in new tab

--- components.js
+++ components.js
@@ -10,15 +10,15 @@ class Header extends HTMLElement {
                         <div class="hover">
                             <a href="#" class="menuItem">Products</a>
                             <div class="list">
-                                <a href="https://tieofficemates.com/shop-ticket-holders/heavy-duty-stitched-job-ticket-holders/" target="_blank">Job Ticket Holders</a>
-                                <a href="https://tieofficemates.com/vinyl-document-sleeves/waterproof-press-lock-seal/" target="_blank">Waterproof Holders</a>
-                                <a href="https://tieofficemates.com/color-coding-options/" target="_blank">Color Coded Products</a>
-                                <a href="https://greatmenucovers.com/cafe-menu-covers-vinyl-trim" target="_blank">Café Menu Covers</a>
-                                <a href="https://greatmenucovers.com/clear-plastic-menu-sleeve-pockets/" target="_blank">Clear Menu Covers</a>
-                                <a href="https://greatmenucovers.com/add-a-menu-page-inserts/" target="_blank">Add-A-Page</a>
-                                <a href="https://tieofficemates.com/auto-repair-order-holders/" target="_blank">Service Order Holders</a>
-                                <a href="https://tieofficemates.com/easy-sign-holders/" target="_blank">Sign Holders</a>
-                                <a href="https://tieofficemates.com/vinyl-tag-holders/" target="_blank">Tag Holders</a>
+                                <a href="https://tieofficemates.com/shop-ticket-holders/heavy-duty-stitched-job-ticket-holders/">Job Ticket Holders</a>
+                                <a href="https://tieofficemates.com/vinyl-document-sleeves/waterproof-press-lock-seal/">Waterproof Holders</a>
+                                <a href="https://tieofficemates.com/color-coding-options/">Color Coded Products</a>
+                                <a href="https://greatmenucovers.com/cafe-menu-covers-vinyl-trim">Café Menu Covers</a>
+                                <a href="https://greatmenucovers.com/clear-plastic-menu-sleeve-pockets/">Clear Menu Covers</a>
+                                <a href="https://greatmenucovers.com/add-a-menu-page-inserts/">Add-A-Page</a>
+                                <a href="https://tieofficemates.com/auto-repair-order-holders/">Service Order Holders</a>
+                                <a href="https://tieofficemates.com/easy-sign-holders/">Sign Holders</a>
+                                <a href="https://tieofficemates.com/vinyl-tag-holders/">Tag Holders</a>
                             </div>
                         </div>
                     </li>
