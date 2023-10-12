62e1829f48000bced1b2f304691723ee6f46e707
brocjac <brockwell2001@gmail.com>
2023-10-12 18:59:26 -0500

    fixed hamburger menu bug when closing menu elements will overlap each other in mobile

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 const menu = document.querySelector(".nav");
-const menuItem = document.querySelectorAll(".menuItem");
 const hamburger = document.querySelector(".hamburger");
 const closeIcon = document.querySelector(".closeIcon");
 const menuIcon = document.querySelector(".menuIcon");
 function toggleMenu() {
-    if (menu.classList.contains("showMenu")) {
-        menu.classList.remove("showMenu");
-        closeIcon.style.display = "none";
-        menuIcon.style.display = "block";
-    } else {
-        menu.classList.add("showMenu");
-        closeIcon.style.display = "block";
-        menuIcon.style.display = "none";
-    }
+    const click1 = Array.from(document.getElementsByClassName("list-1"));
+    Array.prototype.globalForEach = function(callback) {
+        this.forEach(callback);
+    };
+    click1.globalForEach((element) => {
+        const click = document.getElementsByClassName("list-2");
+        Array.from(click).forEach((click2) => {
+            if (menu.classList.contains("showMenu")) {
+                menu.classList.remove("showMenu");
+                element.style.display = "none";
+                click2. style.display = "none";
+                closeIcon.style.display = "none";
+                menuIcon.style.display = "block";
+            } else {
+                menu.classList.add("showMenu");
+                closeIcon.style.display = "block";
+                menuIcon.style.display = "none";
+            }
+        })
+    })
 }
 hamburger.addEventListener("click", toggleMenu);
-// menuItem.forEach(function (menuItem){
-//     menuItem.addEventListener("click", toggleMenu);
-// })
 $(function(){
     $("a.counter").click(function()
     {
