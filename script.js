6d19db71197796ce96633492aa5dd87b1a5dcd69
brocjac <brockwell2001@gmail.com>
2023-10-17 13:35:50 -0500

    started doing JS styling for mobile

--- script.js
+++ script.js
@@ -31,27 +31,69 @@ $(function(){
         return false; // prevent default browser refresh on "#" link
     });
 });
+// function show_hide_1() {
+//     const clickHide = Array.from(document.getElementsByClassName("list-2"));
+//     Array.prototype.globalForEach = function(callback) {
+//         this.forEach(callback);
+//     };
+//     clickHide.globalForEach((element) => {
+//         const click = document.getElementsByClassName("list-1");
+//         Array.from(click).forEach((x) => {
+//             if (x.style.display === "none" && element.style.display==="none") {
+//                 x.style.display = "block";
+//                 console.log(x)
+//             } else if (element.style.display === "block") {
+//                 element.style.display = "none"
+//                 x.style.display = "block";
+//                 console.log(element)
+//             } else {
+//                 x.style.display = "none";
+//             }
+//         });
+//     })
+// }
 function show_hide_1() {
     const clickHide = Array.from(document.getElementsByClassName("list-2"));
     Array.prototype.globalForEach = function(callback) {
         this.forEach(callback);
     };
+    function checkScreenSize() {
+        const menuStyle = Array.from(document.querySelectorAll(".menuItem"));
+        Array.prototype.globalForEach = function(callback) {
+            this.forEach(callback);
+        };
+        var matchMedia = window.matchMedia("(max-width: 420px)");
+        clickHide.globalForEach((menuStyle) => {
+            const click = document.getElementsByClassName("list-1");
+            const item = document.querySelectorAll('.menuItem')
+            Array.from(click).forEach((x) => {
+                if (matchMedia.matches) {
+                    item.classList.remove('large-screen');
+                    item.classList.add('small-screen');
+                } else {
+                    item.classList.remove('small-screen');
+                    item.classList.add('large-screen');
+                }
+            });
+        });
+    }
+    checkScreenSize();
+    window.addEventListener('resize', checkScreenSize);
     clickHide.globalForEach((element) => {
         const click = document.getElementsByClassName("list-1");
         Array.from(click).forEach((x) => {
-            if (x.style.display === "none" && element.style.display==="none") {
+            if (x.style.display === "none" && element.style.display === "none") {
                 x.style.display = "block";
-                console.log(x)
             } else if (element.style.display === "block") {
                 element.style.display = "none"
                 x.style.display = "block";
-                console.log(element)
             } else {
                 x.style.display = "none";
             }
         });
-    })
+    });
 }
+show_hide_1();
 function show_hide_2() {
     const clickHide = Array.from(document.getElementsByClassName("list-1"));
     Array.prototype.globalForEach = function(callback) {
@@ -62,11 +104,9 @@ function show_hide_2() {
         Array.from(click).forEach((y) => {
             if (y.style.display === "none" && element.style.display==="none") {
                 y.style.display = "block";
-                console.log(y)
             } else if (element.style.display === "block") {
                 element.style.display = "none"
                 y.style.display = "block";
-                console.log(element)
             } else {
                 y.style.display = "none";
             }
